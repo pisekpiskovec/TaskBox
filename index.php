3eb20fa5c6040d41fdaa3a0a720b89137e5ca243
PisekPiskovec <pisekpiskovec@gmail.com>
2025-03-19 22:32:33 +0100

    User

--- index.php
+++ index.php
@@ -4,6 +4,9 @@ require("./vendor/autoload.php");
 
 $fw = Base::instance();
 
+$db = new \DB\SQL('mysql:host=localhost;port=3306;dbname=taskbox;charset=utf8', 'root', '', [PDO::ATTR_STRINGIFY_FETCHES => false]);
+$fw->set('DB', $db);
+
 $fw->CACHE = true;
 $fw->set('AUTOLOAD', "app/");
 
@@ -13,6 +16,7 @@ $Cache->exists('route-cache', $routes);
 if(empty($routes)){
     $fw->route('GET /', function($fw) {echo "h";});
     $fw->route('GET /@controller/@action', 'Controllers\@controller->@action');
+    $fw->route('POST /user/add/@username/@email/@password','Controllers\User->addUser');
     $fw->route('GET|POST /robots.txt', 'Controllers\Index->robots_txt');
     $Cache->set('route-cahce', $fw->get('ROUTES'), 86400);
 } else {
