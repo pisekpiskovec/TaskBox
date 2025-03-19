ea496d0a7e8787e841ecdfca4761c52f802efb93
PisekPiskovec <pisekpiskovec@gmail.com>
2025-03-19 21:23:43 +0100

    robots.txt

---
+++ app/Controllers/Index.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Controllers;
+
+class Index {
+	public function robots_txt(\Base $base, array $args = []): void {
+		echo "DO YOU LOVE THE CITY YOU LIVE IN?";
+	}
+}
--- index.php
+++ index.php
@@ -12,6 +12,8 @@ $Cache->exists('route-cache', $routes);
 
 if(empty($routes)){
     $fw->route('GET /', function($fw) {echo "h";});
+    $fw->route('GET /@controller/@action', 'Controllers\@controller->@action');
+    $fw->route('GET|POST /robots.txt', 'Controllers\Index->robots_txt');
     $Cache->set('route-cahce', $fw->get('ROUTES'), 86400);
 } else {
     $fw->set('ROUTES', $routes);
