b787c5996f78f852bacf366a506527b07e926e90
PisekPiskovec <pisekpiskovec@gmail.com>
2025-03-19 21:23:30 +0100

    Route caching

--- index.php
+++ index.php
@@ -3,5 +3,18 @@
 require("./vendor/autoload.php");
 
 $fw = Base::instance();
-$fw->route('GET /', function($fw) {echo "h";});
+
+$fw->CACHE = true;
+$fw->set('AUTOLOAD', "app/");
+
+$Cache = new Cache();
+$Cache->exists('route-cache', $routes);
+
+if(empty($routes)){
+    $fw->route('GET /', function($fw) {echo "h";});
+    $Cache->set('route-cahce', $fw->get('ROUTES'), 86400);
+} else {
+    $fw->set('ROUTES', $routes);
+}
+
 $fw->run();
