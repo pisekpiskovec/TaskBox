2fdf9f147150e8450d61c56cc3d4b4346ad0871c
PisekPiskovec <pisekpiskovec@gmail.com>
2025-03-20 22:55:41 +0100

    Remove caching

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 require("./vendor/autoload.php");
-$fw = Base::instance();
+$fw = \Base::instance();
 $fw->config("./app/Configs/config.ini");
 
 $fw->set('DB', new \DB\SQL(
@@ -9,23 +9,5 @@ $fw->set('DB', new \DB\SQL(
     $fw->get('db.password'),
     [PDO::ATTR_STRINGIFY_FETCHES => false]
 ));
-
-$fw->CACHE = true;
-$fw->set('AUTOLOAD', "app/");
-
-$Cache = new Cache();
-$Cache->exists('route-cache', $routes);
-
-if(empty($routes)){
-    //$fw->route('GET /', function($fw) {echo "h";});
-    //$fw->route('GET /', 'Controllers\Index->index');
-    //$fw->route('GET /login', "Controllers\User->login");
-    //$fw->route('GET /@controller/@action', 'Controllers\@controller->@action');
-    //$fw->route('POST /user/add/@username/@email/@password','Controllers\User->addUser');
-    //$fw->route('GET|POST /robots.txt', 'Controllers\Index->robots_txt');
-    $Cache->set('route-cahce', $fw->get('ROUTES'), 86400);
-} else {
-    $fw->set('ROUTES', $routes);
-}
-
+echo $fw->get('db.dsn');
 $fw->run();
