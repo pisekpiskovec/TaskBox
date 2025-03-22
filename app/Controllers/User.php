308e4e6c745014179e2a4f4cb84aaffe28238797
PisekPiskovec <pisekpiskovec@gmail.com>
2025-03-22 20:21:34 +0100

    User registration form

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -2,20 +2,33 @@
 
 namespace Controllers;
 
-class User{
-    public function login(\Base $base){
-        $base->set('pgTitle', 'Log In');
-        $base->set('content', '/User/login.html');
+class User
+{
+	public function register(\Base $base)
+	{
+		$base->set('pgTitle', 'Register');
+		$base->set('content', '/User/register.html');
 		echo \Template::instance()->render('index.html');
-    }
+	}
+	public function login(\Base $base)
+	{
+		$base->set('pgTitle', 'Log In');
+		$base->set('content', '/User/login.html');
+		echo \Template::instance()->render('index.html');
+	}
 
-    public function addUser(\Base $base, array $args = []){
-        $username = $args['username'];
-        $email = $args['email'];
-        $password = crypt($args['password'], time());
-        
-        
-        $base->get('DB')->exec('INSERT INTO `users` (`username`, `email`, `password`) VALUES (?,?,?)', [ $username, $email, $password]);
-        $base->reroute("/robots.txt", false);
-    }
+	public function addUser(\Base $base, array $args = [])
+	{
+		if ($base->get('POST')['password'] == $base->get('POST')['repeat-password']) {
+			$user = new \Models\User();
+			$tmp = $base->get('POST');
+			$tmp['password'] = password_hash($tmp['password'], PASSWORD_DEFAULT);
+			unset($tmp['repeat-password']);
+			$user->copyfrom($tmp);
+			$user->save();
+			$base->reroute('/login');
+		} else {
+			$base->reroute('/register?e=1');
+		}
+	}
 }
