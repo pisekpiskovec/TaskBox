1bcebe5afee6cb1a0e8c1310cb9b030fbb3c3b01
PisekPiskovec <pisekpiskovec@gmail.com>
2025-04-17 17:21:49 +0200

    Revert "Move db setup to Index"
    This reverts commit bacdfe4d47ba695c938fee16f9c0852bd509be64.

--- app/Controllers/Index.php
+++ app/Controllers/Index.php
@@ -2,12 +2,6 @@
 
 namespace Controllers;
 
-use Models\Login;
-use Models\User;
-use Models\Task;
-use Models\Subtask;
-use Models\Lists;
-
 class Index
 {
 	public function index(\Base $base)
@@ -19,21 +13,4 @@ class Index
 	{
 		echo "DO YOU LOVE THE CITY YOU LIVE IN?";
 	}
-
-	public function install()
-    {
-        User::setdown();
-        Login::setdown();
-        Task::setdown();
-        Subtask::setdown();
-        Lists::setdown();
-        
-        User::setup();
-        Login::setup();
-        Task::setup();
-        Subtask::setup();
-        Lists::setup();
-
-        \Base::instance()->reroute('/');
-    }
 }
---
+++ app/Controllers/Install.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Controllers;
+
+use Models\Login;
+use Models\User;
+use Models\Task;
+use Models\Subtask;
+use Models\Lists;
+use Models\Abnos;
+
+class Install
+{
+    public function setup()
+    {
+        User::setdown();
+        Login::setdown();
+        Task::setdown();
+        Subtask::setdown();
+        Lists::setdown();
+        
+        User::setup();
+        Login::setup();
+        Task::setup();
+        Subtask::setup();
+        Lists::setup();
+
+        \Base::instance()->reroute('/');
+    }
+}
