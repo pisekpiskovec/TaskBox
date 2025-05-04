1f869ec1c5da7a823d25ba90e26dc62da1181900
PisekPiskovec <pisekpiskovec@gmail.com>
2025-05-04 23:20:28 +0200

    Stats and Users

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -10,7 +10,20 @@ class Admin
             \Flash::instance()->addMessage("You must be logged in as an admin", 'danger');
             $base->reroute('/');
         }
+
+        $abnoModel = new \Models\Abnos();
+        $listModel = new \Models\Lists();
+        $subtaskModel = new \Models\Subtask();
+        $taskModel = new \Models\Task();
+        $userModel = new \Models\User();
+
         $base->set('pgTitle', 'Dashboard');
+        $base->set('abnosCount', $abnoModel->count());
+        $base->set('listsCount', $listModel->count());
+        $base->set('subtasksCount', $subtaskModel->count());
+        $base->set('tasksCount', $taskModel->count());
+        $base->set('usersCount', $userModel->count());
+
         $base->set('content', '/Admin/dashboard.html');
         echo \Template::instance()->render('index.html');
     }
