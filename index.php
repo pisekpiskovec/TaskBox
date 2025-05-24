6597b8794b4a1c93be245f8fa5293fc71b5dfc16
PisekPiskovec <pisekpiskovec@gmail.com>
2025-05-24 22:56:16 +0200

    Added more prevention
    Connect only when full details are provided

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require("./vendor/autoload.php");
 $fw = \Base::instance();
 $fw->config("./app/Configs/config.ini");
 
-if ($fw->get('db.dsn')) {
+if ($fw->get('db.dsn') && $fw->get('db.username') && $fw->get('db.password')) {
     $fw->set('DB', new \DB\SQL(
         $fw->get('db.dsn'),
         $fw->get('db.username'),
